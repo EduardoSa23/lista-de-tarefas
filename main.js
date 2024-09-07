af7ae4e45128b193a488412710b1b686ee0d964c
EduardoSa23 <eduardo.sa2398@gmail.com>
2024-09-07 09:31:02 -0300

    adcionando a funcao limpar os campos ul ao clicar no botao cancelar

--- main.js
+++ main.js
@@ -1,25 +1,27 @@
 $(document).ready(function() {
     $('header button').click(function() {
         $('form').slideDown();
-     })
+    })
  
-     $('#btn-cancelar').click(function() {
-         $('form').slideUp();
-      })
+        $('#btn-cancelar').click(function() {
+            $('form').slideUp();
+            $('ul').empty();
+        })
 
-      $('form').on('submit', function(e) {
-        e.preventDefault()
-        const nomeTarefa = $('#nome-tarefa').val()
-        const novoItem = $('<li style="display: none"></li>');
+        $('form').on('submit', function(e) {
+            e.preventDefault()
+            const nomeTarefa = $('#nome-tarefa').val()
+            const novoItem = $('<li style="display: none"></li>');
 
-        $(`<li>${nomeTarefa}</li>`).appendTo(novoItem);
+            $(`<li>${nomeTarefa}</li>`).appendTo(novoItem);
 
-        $(novoItem).appendTo('ul');
-        $(novoItem).fadeIn(1000);
-        $('#nome-tarefa').val('');
-        $(document).on('click', 'li', function() {
-            $(this).toggleClass('riscado');
-        });
-      })
+            $(novoItem).appendTo('ul');
+            $(novoItem).fadeIn(1000);
+            $('#nome-tarefa').val('');
+            $(document).on('click', 'li', function() {
+                $(this).toggleClass('riscado');
+            });
+
+        })
 
 })
